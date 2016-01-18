1144f042029aa9b0413a33b35207acff5e87354d
Rebouh Aymen <aymenworks@gmail.com>
2016-01-18 20:07:26 +0100

    Add simple getters for the middle width a,d height of any views
    Just to avoid using the long `self.view.bounds....`.

---
+++ MentorAndMentees/MentorAndMentees/Extensions/UIViewExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewExtension.swift
+//  MentorAndMentees
+//
+//  Created by Rebouh Aymen on 17/01/2016.
+//  Copyright © 2016 Mentor And Mentees. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+  var midHeight: CGFloat {
+    return bounds.height/2.0
+  }
+  
+  var midWidth: CGFloat {
+    return bounds.width/2.0
+  }
+}
