18d3d55e9f9ca12ab24552da044a5475e148cca4
Rebouh Aymen <aymenworks@gmail.com>
2016-01-18 20:08:22 +0100

    Add unwind segue for all UIViewController
    The pop/dimiss segue is now possible directly form the storyboard

---
+++ MentorAndMentees/MentorAndMentees/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewControllerExtension.swift
+//  MentorAndMentees
+//
+//  Created by Rebouh Aymen on 17/01/2016.
+//  Copyright © 2016 Mentor And Mentees. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+  @IBAction func unwind(segue: UIStoryboardSegue) {
+    
+  }
+}
