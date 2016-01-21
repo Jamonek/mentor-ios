2e44bf1957b81a659900ef3c52559432b84c72cc
Rebouh Aymen <aymenworks@gmail.com>
2016-01-21 20:46:21 +0100

    Add menu controller options (delete, rename, more) for the group collection item

--- MentorAndMentees/MentorAndMentees/Views/GroupItemCollectionViewCell.swift
+++ MentorAndMentees/MentorAndMentees/Views/GroupItemCollectionViewCell.swift
@@ -16,6 +16,22 @@ class GroupItemCollectionViewCell: UICollectionViewCell {
   @IBOutlet weak var nameLabel: UILabel!
   @IBOutlet weak var membersLabel: UILabel!
   @IBOutlet weak var lastActivityLabel: UILabel!
+  @IBOutlet weak var moreButton: UIButton!
+  
+  private struct MenuControllerOptions {
+    static let deleteGroup: Selector      = "deleteGroup";
+    static let renameGroup: Selector      = "renameGroup";
+    static let moreInformations: Selector = "moreInformations";
+  }
+  
+  lazy var optionsMenuController: UIMenuController = {
+    let menu = UIMenuController.sharedMenuController()
+    menu.menuItems = [UIMenuItem(title: Localizable("Delete"), action: MenuControllerOptions.deleteGroup),
+                      UIMenuItem(title: Localizable("Rename"), action: MenuControllerOptions.renameGroup),
+                      UIMenuItem(title: Localizable("More"), action: MenuControllerOptions.moreInformations)]
+  
+    return menu
+  }()
   
   // MARK: - Lifecycle -
   
@@ -25,4 +41,32 @@ class GroupItemCollectionViewCell: UICollectionViewCell {
     layer.cornerRadius = 4.0
   }
 
+  // MARK: - User Interaction -
+  
+  override func canPerformAction(action: Selector, withSender sender: AnyObject?) -> Bool {
+    return action == MenuControllerOptions.deleteGroup || action == MenuControllerOptions.renameGroup || action == MenuControllerOptions.moreInformations
+  }
+  
+  override func canBecomeFirstResponder() -> Bool {
+    return true
+  }
+  
+  @IBAction func showMenu(sender: AnyObject) {
+    becomeFirstResponder()
+    
+    optionsMenuController.setTargetRect(moreButton.frame, inView: self)
+    optionsMenuController.setMenuVisible(true, animated: true)
+  }
+  
+  func deleteGroup() {
+    // TODO: -
+  }
+  
+  func renameGroup() {
+    // TODO: -
+  }
+  
+  func moreInformations() {
+    // TODO: -
+  }
 }
